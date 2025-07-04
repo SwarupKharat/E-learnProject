6d6fa2111b9210c3dbcf8e93f39eea9e00c0c407
SwarupKharat <swaroopkharat18@gmail.com>
2025-07-04 19:44:50 +0530

    Fix: Add Tailwind setup for Render

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
+import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+// import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import rootReducer from "./reducer";
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import { Toaster } from "react-hot-toast";
-const store=configureStore({
-  reducer:rootReducer,
+const store = configureStore({
+  reducer: rootReducer,
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <App />
-      <Toaster/>
+        <App />
+        <Toaster />
       </BrowserRouter>
     </Provider>
 
